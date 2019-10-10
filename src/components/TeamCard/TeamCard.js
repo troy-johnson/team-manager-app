c8650f1d6cbcef311af2f94a9fa1c75556528ee2
Troy Johnson <troy.johnson@berkadia.com>
2019-10-10 14:00:02 -0600

    #14: updated dashboard and teamcard

--- src/components/TeamCard/TeamCard.js
+++ src/components/TeamCard/TeamCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, Image, Placeholder, Statistic } from 'semantic-ui-react';
+import { Button, Card, Image, Placeholder } from 'semantic-ui-react';
 import './TeamCard.css';
 
 const TeamCard = props => {
@@ -7,7 +7,43 @@ const TeamCard = props => {
 
   return (
     <Card>
-      Team here
+      {team.loaded ? (
+        <Card.Content>
+          <Image src="" />
+        </Card.Content>
+      ) : (
+        <Card.Content>
+          <Placeholder className="dashboard-placeholder-image">
+            <Placeholder.Image rectangular />
+          </Placeholder>
+        </Card.Content>
+      )}
+      {team.loaded ? (
+        <Card.Content>
+          <Card.Header>Team Name</Card.Header>
+          <Card.Meta>Team Sport</Card.Meta>
+          <Card.Description>Next Game</Card.Description>
+        </Card.Content>
+      ) : (
+        <Card.Content>
+          <Placeholder>
+            <Placeholder.Line length="short" />
+            <Placeholder.Line length="very long" />
+            <Placeholder.Line length="medium" />
+          </Placeholder>
+        </Card.Content>
+      )}
+      <Card.Content extra>
+        <div className="ui two buttons">
+          <Button
+            disabled={!team.loaded}
+            color={team.status === 'in' ? 'green' : 'grey'}
+          >
+            VIEW
+          </Button>
+        </div>
+        {/* )} */}
+      </Card.Content>
     </Card>
   );
 };
