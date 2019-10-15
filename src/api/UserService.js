ab33a868aff1f81acb27654842efcf9216ff267f
Ben Nelson <ben.nelson@berkadia.com>
2019-10-15 16:55:28 -0600

    #13 added new http client to user service

--- src/api/UserService.js
+++ src/api/UserService.js
@@ -1,11 +1,5 @@
-export const test = async (params, token) => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1', {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
-
+export const test = async (params, http) => {
+  const response = await http.get('https://jsonplaceholder.typicode.com/todos/1')
   if (response) {
     return response.json();
   } else {
